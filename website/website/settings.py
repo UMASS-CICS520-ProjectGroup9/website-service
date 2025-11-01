dc5f9fc62bbb4fbbe5cbe5a1c9a660c9edecb595
Ping <ping58972@gmail.com>
2025-11-01 18:44:43 -0400

    add index and events page

--- website/website/settings.py
+++ website/website/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'livereload',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- website/website/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'index.html')
