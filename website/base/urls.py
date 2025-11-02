a04d9c41bc7a9303f8a48156dd7e9e29f906ae26
Ping <ping58972@gmail.com>
2025-11-01 23:40:42 -0400

    working on template

--- website/base/urls.py
+++ website/base/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('events/', views.events, name='events'),
+    path('discussions/', views.discussions, name='discussions'),
+    path('courses/', views.courses, name='courses'),
+    path('professors/', views.professors, name='professors'),
+    path('myplan/', views.myplan, name='myplan'),   
 ]
--- website/base/views.py
+++ website/base/views.py
@@ -5,4 +5,19 @@ def index(request):
     return render(request, 'index.html')
 
 def events(request):
-    return render(request, 'events.html')
+    return render(request, 'pages/events/events.html')
+
+def discussions(request):
+    return render(request, 'pages/discussions/discussions.html')
+
+def courses(request):
+    return render(request, 'pages/courses/courses.html')
+
+def professors(request):
+    return render(request, 'pages/professors/professors.html')
+
+def myplan(request):
+    return render(request, 'pages/myplan/myplan.html')
+
+def myplan(request):
+    return render(request, 'pages/myplan/myplan.html')
