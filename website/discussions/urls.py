39940956f974275968eea7a7db6e4b5f4296d7ce
Isaac <isaac@Isaacs-MacBook-Air.local>
2025-11-27 11:04:10 -0500

    Add new comments

--- website/discussions/urls.py
+++ website/discussions/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.discussion_list, name='discussions'),
     path('create/', views.discussion_create, name='discussion_create'),
+    path('<int:pk>/comment/', views.comment_create, name='discussion_comment_create'),
     path('<int:pk>/', views.discussion_detail, name='discussion_detail'),
 ]
